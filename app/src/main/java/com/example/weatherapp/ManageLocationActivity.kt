17066d2666d3bbad6c06f792a5503ab1d8dab68c
MenukaRanasinghe <hgmranasinghe@gmail.com>
2023-11-29 15:31:39 +0530

    Initial commit

---
+++ app/src/main/java/com/example/weatherapp/ManageLocationActivity.kt
@@ -0,0 +1,11 @@
+package com.example.weatherapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ManageLocationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_manage_location)
+    }
+}
